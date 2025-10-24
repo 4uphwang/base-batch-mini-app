6c2db3dee3d1d9ac779620ba6771fd70235cc6a7
Jeongseup <seup87@gmail.com>
2025-10-25 08:59:25 +0900

    fix show mycard at main home #check

--- components/main/MainHome.tsx
+++ components/main/MainHome.tsx
@@ -10,13 +10,15 @@ import CardCollectionAdder from "./CardCollectionAdder";
 import CollectCardsSection from "./CollectCardsSection";
 import HeroSection from "./HeroSection";
 import MyCardSection from "./MyCardSection";
+import { useAccount } from "wagmi";
 
 const ACTION_ADD_CARD = "addCardCollection";
 
 export default function MainHome() {
     const router = useRouter();
     useBaseCardNFTs();
-    const { data: card } = useMyCard();
+    const { address } = useAccount();
+    const { data: card } = useMyCard(address);
 
     // 1. 딥링크 파라미터 추출
     const { action, cardId } = useMiniappParams();
@@ -27,10 +29,9 @@ export default function MainHome() {
 
     return (
         <div className="min-h-screen bg-white">
-            {
-                action === ACTION_ADD_CARD && cardId &&
+            {action === ACTION_ADD_CARD && cardId && (
                 <CardCollectionAdder collectedCardId={cardId} />
-            }
+            )}
             {card ? (
                 <div className="flex flex-col">
                     <MyCardSection />
